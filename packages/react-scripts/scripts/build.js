41e51d34796083011da2e5787959c107b17c9dcf
Steven Em <sem@dealersocket.com>
2020-01-30 15:16:49 -0800

    Remove warning over conflicting order of css files

--- packages/react-scripts/scripts/build.js
+++ packages/react-scripts/scripts/build.js
@@ -184,7 +184,11 @@ function build(previousFileSizes) {
         });
       } else {
         messages = formatWebpackMessages(
-          stats.toJson({ all: false, warnings: true, errors: true })
+          stats.toJson({
+            all: false,
+            warnings: true,
+            errors: true,
+          })
         );
       }
       if (messages.errors.length) {
@@ -194,20 +198,6 @@ function build(previousFileSizes) {
           messages.errors.length = 1;
         }
         return reject(new Error(messages.errors.join('\n\n')));
-      }
-      if (
-        process.env.CI &&
-        (typeof process.env.CI !== 'string' ||
-          process.env.CI.toLowerCase() !== 'false') &&
-        messages.warnings.length
-      ) {
-        console.log(
-          chalk.yellow(
-            '\nTreating warnings as errors because process.env.CI = true.\n' +
-              'Most CI servers set it automatically.\n'
-          )
-        );
-        return reject(new Error(messages.warnings.join('\n\n')));
       }
 
       return resolve({
